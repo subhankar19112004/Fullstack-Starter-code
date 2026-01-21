78b54e166e03b6268343fcf7049057a985cc56c8
Subhankar Jena <subhankarjenaofficial19@gmail.com>
2026-01-21 11:24:04 +0530

    starter codes

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className=" text-amber-300 font-extrabold text-8xl">
+      <p className="text-green-400">Starter Code Folder</p>Tailwind, rect-touter
+      installed
+    </div>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,1 @@
+# Fullstack-Starter-code
